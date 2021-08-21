25dbc626dcb59919ca7edadc51c78da79f60cdbb
Tikhon <work@ivanovtd.ru>
2021-08-21 18:37:30 +0300

    Delete hms_auth directory

--- hms_auth/connect_ssh.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-def connect_ssh(web, home):
-    """Подключается к серверу, не эмулируя термина, nano и прочее работают корректно"""
-    os.system(f"ssh -q -t {web} -- 'set +o history; sudo --stdin --validate --prompt='' <<< {os.environ['MJ_WEB_ROOT']}; exec -a sh sudo bash -c \"cd {home} && sudo -s\"'")
-
